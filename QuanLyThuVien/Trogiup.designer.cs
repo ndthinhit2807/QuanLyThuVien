4ea8a943bd1d3cd4f240607b27ef7118f9558df5
ndthinhit2807 <75533943+ndthinhit2807@users.noreply.github.com>
2021-01-22 22:45:20 +0700

    xoá tab page trợ giúp

--- QuanLyThuVien/Trogiup.designer.cs
+++ QuanLyThuVien/Trogiup.designer.cs
@@ -31,22 +31,22 @@ namespace QuanLyThuVien
         {
             this.xtraGioithieu = new DevExpress.XtraTab.XtraTabPage();
             this.panelGioiThieu = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.btnCapnhat = new System.Windows.Forms.Button();
-            this.prgKiemTra = new System.Windows.Forms.ProgressBar();
-            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
+            this.prgKiemTra = new System.Windows.Forms.ProgressBar();
+            this.btnCapnhat = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label11 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
             this.xtraGioithieu.SuspendLayout();
             this.panelGioiThieu.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -58,7 +58,7 @@ namespace QuanLyThuVien
             // 
             this.xtraGioithieu.Controls.Add(this.panelGioiThieu);
             this.xtraGioithieu.Name = "xtraGioithieu";
-            this.xtraGioithieu.Size = new System.Drawing.Size(1533, 935);
+            this.xtraGioithieu.Size = new System.Drawing.Size(1533, 958);
             this.xtraGioithieu.Text = "Giới Thiệu";
             // 
             // panelGioiThieu
@@ -75,63 +75,71 @@ namespace QuanLyThuVien
             this.panelGioiThieu.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelGioiThieu.Location = new System.Drawing.Point(0, 0);
             this.panelGioiThieu.Name = "panelGioiThieu";
-            this.panelGioiThieu.Size = new System.Drawing.Size(1533, 935);
+            this.panelGioiThieu.Size = new System.Drawing.Size(1533, 958);
             this.panelGioiThieu.TabIndex = 4;
             // 
-            // label1
+            // progressBar1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(450, 127);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(639, 65);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Phần Mềm Quản Lý Thư Viện";
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.progressBar1.Location = new System.Drawing.Point(983, 326);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(256, 27);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Visible = false;
             // 
-            // groupBox1
+            // prgKiemTra
             // 
-            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.groupBox1.Controls.Add(this.label11);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(426, 382);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(686, 227);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Tác Giả";
+            this.prgKiemTra.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.prgKiemTra.Location = new System.Drawing.Point(637, 341);
+            this.prgKiemTra.Name = "prgKiemTra";
+            this.prgKiemTra.Size = new System.Drawing.Size(256, 27);
+            this.prgKiemTra.TabIndex = 9;
+            this.prgKiemTra.Visible = false;
             // 
-            // label2
+            // btnCapnhat
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 42);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(230, 30);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Trần Phan Minh Trường";
+            this.btnCapnhat.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCapnhat.Location = new System.Drawing.Point(686, 290);
+            this.btnCapnhat.Name = "btnCapnhat";
+            this.btnCapnhat.Size = new System.Drawing.Size(166, 27);
+            this.btnCapnhat.TabIndex = 8;
+            this.btnCapnhat.Text = "Kiểm Tra Cập Nhật";
+            this.btnCapnhat.UseVisualStyleBackColor = true;
+            this.btnCapnhat.Click += new System.EventHandler(this.btnCapnhat_Click);
             // 
-            // label4
+            // linkLabel1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(159, 111);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(187, 30);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Nguyễn Đức Thịnh";
+            this.linkLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(543, 794);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(400, 21);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.facebook.com/groups/1136402743497604";
             // 
-            // label5
+            // label7
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(169, 180);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(177, 30);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Nguyễn Văn Nam";
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(422, 794);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 21);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Nhóm hỗ trợ:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(667, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(205, 21);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Phiên Bản: 1.0 (25/01/2021)";
             // 
             // listBox1
             // 
@@ -149,95 +157,45 @@ namespace QuanLyThuVien
             "➝  Quản lý độc giả",
             "➝  Quản lý mượn trả ",
             "➝  Thống kê"});
-            this.listBox1.Location = new System.Drawing.Point(426, 636);
+            this.listBox1.Location = new System.Drawing.Point(426, 648);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(686, 124);
             this.listBox1.TabIndex = 2;
             // 
-            // label6
+            // groupBox1
             // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(667, 217);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(205, 21);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Phiên Bản: 1.0 (25/01/2021)";
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(426, 394);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(686, 227);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tác Giả";
             // 
-            // label7
+            // label11
             // 
-            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(422, 782);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(104, 21);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "Nhóm hỗ trợ:";
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(390, 180);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 30);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "1811062212 ";
             // 
-            // linkLabel1
+            // label5
             // 
-            this.linkLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(543, 782);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(400, 21);
-            this.linkLabel1.TabIndex = 5;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://www.facebook.com/groups/1136402743497604";
-            // 
-            // btnCapnhat
-            // 
-            this.btnCapnhat.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnCapnhat.Location = new System.Drawing.Point(686, 278);
-            this.btnCapnhat.Name = "btnCapnhat";
-            this.btnCapnhat.Size = new System.Drawing.Size(166, 27);
-            this.btnCapnhat.TabIndex = 8;
-            this.btnCapnhat.Text = "Kiểm Tra Cập Nhật";
-            this.btnCapnhat.UseVisualStyleBackColor = true;
-            this.btnCapnhat.Click += new System.EventHandler(this.btnCapnhat_Click);
-            // 
-            // prgKiemTra
-            // 
-            this.prgKiemTra.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.prgKiemTra.Location = new System.Drawing.Point(637, 329);
-            this.prgKiemTra.Name = "prgKiemTra";
-            this.prgKiemTra.Size = new System.Drawing.Size(256, 27);
-            this.prgKiemTra.TabIndex = 9;
-            this.prgKiemTra.Visible = false;
-            // 
-            // xtraTabControl1
-            // 
-            this.xtraTabControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.xtraTabControl1.BorderStylePage = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.xtraTabControl1.Location = new System.Drawing.Point(0, 0);
-            this.xtraTabControl1.Name = "xtraTabControl1";
-            this.xtraTabControl1.SelectedTabPage = this.xtraGioithieu;
-            this.xtraTabControl1.Size = new System.Drawing.Size(1535, 960);
-            this.xtraTabControl1.TabIndex = 4;
-            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
-            this.xtraGioithieu});
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.progressBar1.Location = new System.Drawing.Point(983, 314);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(256, 27);
-            this.progressBar1.TabIndex = 9;
-            this.progressBar1.Visible = false;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(390, 42);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(129, 30);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "1811061230 ";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(169, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 30);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Nguyễn Văn Nam";
             // 
             // label10
             // 
@@ -248,14 +206,57 @@ namespace QuanLyThuVien
             this.label10.TabIndex = 0;
             this.label10.Text = "1811060740";
             // 
-            // label11
+            // label4
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(390, 180);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(129, 30);
-            this.label11.TabIndex = 0;
-            this.label11.Text = "1811062212 ";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 30);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nguyễn Đức Thịnh";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(390, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 30);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "1811061230 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Trần Phan Minh Trường";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(450, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(639, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phần Mềm Quản Lý Thư Viện";
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.xtraTabControl1.BorderStylePage = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.xtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xtraTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.SelectedTabPage = this.xtraGioithieu;
+            this.xtraTabControl1.ShowTabHeader = DevExpress.Utils.DefaultBoolean.False;
+            this.xtraTabControl1.Size = new System.Drawing.Size(1535, 960);
+            this.xtraTabControl1.TabIndex = 4;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtraGioithieu});
             // 
             // frmTrogiup
             // 
